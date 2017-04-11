9e448a018f91c0fd125fdd98559ca6079ed3276e
ccadars <ccadars.beweb@gmail.com>
2017-04-11 11:47:03 +0200

    implementation express + tests

--- serveur.js
+++ serveur.js
@@ -1,15 +1,13 @@
-const http = require ('http');
-const hostname = '127.0.0.1';
-const port = 12107;
+var express = require('express');
+var appli = express();
 
-const serveur = http.createServer(
-  function(request,response) {
-    response.statusCode = 200;
-    response.setHeader('Content-Type','text/plain');
-    response.end('Je suis beaucoup plus lent que Paul');
-  }
+appli.get(
+	'/',
+	function(request,response){
+		response.send('Hello world');
+	}
 );
 
-serveur.listen(port,hostname,function(){
-  console.log('Le serveur écoute là-bas : ' + hostname + " : " + port);
+appli.listen(12107, function(){
+	console.log("Pour péter dans la doc il faut de l'Heroku");
 });
