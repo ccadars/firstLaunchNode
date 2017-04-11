76d5ecce3549f480a071fca5936a2eb709411f48
ccadars <ccadars.beweb@gmail.com>
2017-04-11 12:00:27 +0200

    ajout de vue sur /

--- serveur.js
+++ serveur.js
@@ -4,9 +4,19 @@ var appli = express();
 appli.get(
 	'/',
 	function(request,response){
-		response.send('Hello world');
+		response.sendFile(__dirname + '/views/index.html');
 	}
 );
+
+appli.get(
+	'/user',
+	function(request,response){
+		var retour = {
+			nom : "cadars",
+			prenom : "clement"
+		};
+		response.json(retour);
+});
 
 appli.listen(12107, function(){
 	console.log("Pour péter dans la doc il faut de l'Heroku");
